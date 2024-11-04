a2dc8b706a728d2d8ce8a28e595e4eb600f658d4
V <tanakakanta@g.ecc.u-tokyo.ac.jp>
2024-11-05 00:23:40 +0800

    hoge

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ void processInput(GLFWwindow *window);
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 800;
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f)); //Initial Position; Direction (0,0,-1)
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
