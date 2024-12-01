1be5a0d6092382d2e1f753634a4277cb6b81adce
V <tanakakanta@g.ecc.u-tokyo.ac.jp>
2024-12-01 15:22:57 +0900

    hoge

--- project/hairview/main.cpp
+++ project/hairview/main.cpp
@@ -50,7 +50,7 @@ int main(){
     HairModel model;
     std::string err, warn;
 
-    bool ret = loader.LoadFromFile(&model, &err, &warn, MODEL_DIR "/blonde.hair");
+    bool ret = loader.LoadFromFile(&model, &err, &warn, MODEL_DIR "/wCurly.hair");
     if (!ret) {
         std::cerr << "Error: " << err << std::endl;
         if (!warn.empty()) {
